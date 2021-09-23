367ad29fc41cae3aee957f631494407cd1d38b95
jiminjung <33984762+jiminjung@users.noreply.github.com>
2021-09-23 18:20:39 +0900

    Create HelloGit.java

---
+++ src/kr/ac/jbnu/se/awp/gitplay4/HelloGit.java
@@ -0,0 +1,20 @@
+package kr.ac.jbnu.se.awp.gitplay0;
+
+public class HelloGit {
+	public static void main(String[] args) {
+		System.out.println("Hello Git!!");
+		
+		System.out.println("Hello Git ???");
+		
+		// Okay I'll change this part as below:
+		// I've just addd the following part in Eclipse.
+		for (int i = 0; i < 100; i++)
+		{
+			System.out.println("i:" + i);
+		}
+		// I added the following part.
+		System.out.println("This is a confliction test.");
+	}
+	// add comment: This is Hello Git.
+
+}
