fa1c56981daded8f4c8aa3b8f522af5dfa26c55f
jimin <jimin606@naver.com>
2021-09-23 19:39:58 +0900

    jimin's commit test

--- src/kr/ac/jbnu/se/awp/gitplay4/HelloGit.java
+++ src/kr/ac/jbnu/se/awp/gitplay4/HelloGit.java
@@ -14,7 +14,11 @@ public class HelloGit {
 		}
 		// I added the following part.
 		System.out.println("This is a confliction test.");
+		System.out.println("This is a jimin's test.");
+		
+		
 	}
 	// add comment: This is Hello Git.
+	// 1ºü Á¤Áö¹Î¤¾¤¾
 
 }
