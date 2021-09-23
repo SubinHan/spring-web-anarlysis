5d1be33d7154029bb3252d9e3ed2805a6457262d
jiminjung <33984762+jiminjung@users.noreply.github.com>
2021-09-23 20:13:35 +0900

    Update HelloGit.java

--- src/kr/ac/jbnu/se/awp/gitplay4/HelloGit.java
+++ src/kr/ac/jbnu/se/awp/gitplay4/HelloGit.java
@@ -4,7 +4,7 @@ public class HelloGit {
 	public static void main(String[] args) {
 		System.out.println("Hello Git!!");
 		
-		System.out.println("Hello Git 1");
+		System.out.println("Hello Git 2");
 		
 		// Okay I'll change this part as below:
 		// I've just addd the following part in Eclipse.
