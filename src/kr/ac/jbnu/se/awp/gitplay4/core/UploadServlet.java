937dada7cd8e5e62a85986394eb0e0db50cf7e51
cmkxak <gcm466@naver.com>
2021-10-15 03:06:07 +0900

    Add function that manages file by each client's ip

--- src/kr/ac/jbnu/se/awp/gitplay4/core/UploadServlet.java
+++ src/kr/ac/jbnu/se/awp/gitplay4/core/UploadServlet.java
@@ -12,17 +12,21 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
 
+
 @WebServlet(name = "UploadServlet", urlPatterns = { "/UploadServlet" })
 @MultipartConfig(fileSizeThreshold = 1024 * 1024, maxFileSize = 1024 * 1024 * 5, maxRequestSize = 1024 * 1024 * 5 * 5)
 public class UploadServlet extends HttpServlet {
-	private static final String UPLOAD_DIR = "upload_files";
-
+	
+	private String UPLOAD_DIR;
+	private String uploadPath;
+	private CsvManager manager;
+ 	
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-		
+		manager = new CsvManager();
+		UPLOAD_DIR = manager.getUserIP(req);
 		// 업로드 경로
-		String uploadPath = getServletContext().getRealPath("") + File.separator + UPLOAD_DIR;
+		uploadPath = getServletContext().getRealPath("") + File.separator + UPLOAD_DIR;
 		// getServlet().getRealPath("") 단점
 		// - 애플리케이션 배포하다가 기존 서버를 clean하면 업로드폴더가 싹 날라감
 		// - 절대경로로 지정해서 업로드하는게 안전함
@@ -38,12 +42,13 @@ public class UploadServlet extends HttpServlet {
 		try {
 			String fileName = "";
 			for (Part part : req.getParts()) { // req.getParts() : Multipart데이터들을 Part객체로 리턴
-				fileName = getFileName(part);
+				fileName = getFileName(part); //파일명을 가져와주는 메소드 실행 
 				System.out.println(part.getHeader("content-disposition")); // part마다 header 있음
 				if (fileName != null && !"".equals(fileName)) { // !폼필드경우 && !파일아예업로드 안했을 때
 					part.write(uploadPath + File.separator + fileName); // 파일 저장
 					System.out.println("파일명 : " + fileName + "저장완료!!!");
 				}
+				
 			}
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
@@ -70,4 +75,11 @@ public class UploadServlet extends HttpServlet {
 		}
 		return null; // filename이 없는 경우 (폼필드 데이터인 경우):
 	}
+	
+	//* 파일의 경로 반환 
+	public String getFilePath() {	
+		String filePath = this.uploadPath;
+		return filePath;
+	}
+		
 } // class
