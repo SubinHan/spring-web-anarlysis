0dc13904c9023047175ede41f99ba95349475474
jimin <jimin@DESKTOP-8QTHHQ2>
2021-10-18 21:46:07 +0900

    Rename Line, Histogram class to LineGenerator, HistogramGenerator

--- src/kr/ac/jbnu/se/awp/gitplay4/core/HistogramGenerator.java
+++ src/kr/ac/jbnu/se/awp/gitplay4/core/HistogramGenerator.java
@@ -9,33 +9,36 @@ import org.rosuda.REngine.Rserve.RConnection;
 import org.rosuda.REngine.Rserve.RserveException;
 //import javax.servlet.http.HttpServletRequest;
 
-public class Line extends ChartGenerator{
-	
-	public void makeLine() {
+public class HistogramGenerator extends ChartGenerator{
 
+	
+	public void makeHistogram() {
+		
+		//RConnection connection = null;
 		try {
 			connection = new RConnection();
 	
-			connection.eval("csv_data <- read.csv("+rfilePath +",header=TRUE)");
+			connection.eval("csv_data <- read.csv(\""+filePath +"\",header=TRUE)");
 			
 			
 			connection.eval("library(ggplot2)");
 			connection.eval("library(dplyr)");
 			
-			connection.eval("a<-csv_data%>% ggplot(aes("+x+", "+y+", group=1))"
-					+ "+ geom_line(color = 'coral')"
+			connection.eval("a <- csv_data%>%"
+					+ "ggplot(aes("+y+", group=1))"
+					+ "+ geom_bar(color = 'coral')"
 					+ "+ ggtitle(\""+imageTitle+"\")"
 					+ "+ xlab(\""+xName+"\")+ylab(\""+yName+"\")"
 					+ "+ theme_light()"
 					+ "+ theme(plot.title = element_text(size=20, hjust=0.5))");
+			
 			connection.eval("ggsave(filename=\""+imageName+"\", plot=a, width=12,height=6)");
 			
-			System.out.println(imageName + " Line Making Clear");
+			System.out.println(imageName + " Histogram Making Clear");
 
 		} catch (RserveException e) {
 			e.printStackTrace();
-		}  
-		finally {
+		}finally {
 			connection.close();
 		}
 	}
